c80e881f4b831d169c557cd68fa449a30d93298e
Hieu Do <hieu.do@sotatek.com>
2019-10-28 17:52:07 +0700

    add user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+          [
+            'name' => 'user1',
+            'email' => 'email_1@gmail.com',
+            'password' => bcrypt(123456)
+          ]
+        ]);
     }
 }
