5473455a1cf58a7bd9187a8d249aa6d9d5a31653
Hieu Do <hieu.do@sotatek.com>
2019-11-13 21:46:23 +0700

    add material design

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,18 +1,6 @@
-import AuthenticationUtils from './common/AuthenticationUtils';
+
 window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) {}
+window.Popper = require('popper.js').default;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -30,18 +18,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * a simple convenience so we don't have to attach every token manually.
  */
 
- const token = document.head.querySelector('meta[name="csrf-token"]');
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
- if (token) {
-   window.csrf_token = token.content;
-   window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-   window.axios.defaults.headers.common.Authorization = `Bearer ${AuthenticationUtils.getAccessToken()}`;
- } else {
-   console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
- }
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
- window.isAuthenticated = AuthenticationUtils.checkSession() && AuthenticationUtils.isAuthenticated();
- window.isAdmin = AuthenticationUtils.isAdminRole();
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/js/configs/routes.js
+++ resources/js/configs/routes.js
@@ -1,58 +1,51 @@
-import LoginPage from '../pages/LoginPage.vue';
+import DashboardLayout from '@/pages/Layout/DashboardLayout.vue'
 
-import BasePage from '../pages/admin/BasePage.vue';
-import Dashboard from '../pages/admin/Dashboard.vue';
-import StudentPage from '../pages/admin/Student.vue';
-
-import BaseUserPage from '../pages/user/BaseUserPage.vue';
-import UserDashboard from '../pages/user/Dashboard.vue';
-
-import NotFound from '../pages/errors/404.vue';
+import Dashboard from '@/pages/Dashboard.vue'
+import UserProfile from '@/pages/UserProfile.vue'
+import TableList from '@/pages/TableList.vue'
+import Typography from '@/pages/Typography.vue'
+import Icons from '@/pages/Icons.vue'
+import Notifications from '@/pages/Notifications.vue'
 
 export default {
   mode: 'history',
   routes: [
     {
-      path: '/login',
-      name: 'Login',
-      component: LoginPage,
-    },
-    {
       path: '/',
-      component: BaseUserPage,
+      component: DashboardLayout,
+      redirect: '/dashboard',
       children: [
         {
-          path: '/',
-          name: 'UserDashboard',
-          component: UserDashboard,
-          meta: {
-            requiresAuth: true
-          },
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'user',
+          name: 'User Profile',
+          component: UserProfile
+        },
+        {
+          path: 'table',
+          name: 'Table List',
+          component: TableList
+        },
+        {
+          path: 'typography',
+          name: 'Typography',
+          component: Typography
+        },
+        {
+          path: 'icons',
+          name: 'Icons',
+          component: Icons
+        },
+        {
+          path: 'notifications',
+          name: 'Notifications',
+          component: Notifications
         }
       ]
-    },
-    {
-      path: '/admin',
-      component: BasePage,
-      children: [
-        {
-          path: '/',
-          name: 'Dashboard',
-          component: Dashboard,
-          meta: {
-            requiresAdmin: true,
-          },
-        },
-        {
-          path: 'student',
-          name: 'Student',
-          component: StudentPage,
-          meta: {
-            requiresAdmin: true,
-          },
-        },
-        { path: '*', name: '404', component: NotFound }
-      ],
-    },
-  ],
-};
+    }
+  ]
+}
