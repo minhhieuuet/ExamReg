4d35bdd92335dcf85816a7a2625b24c6b8efcc10
nambp <nambp@eway.vn>
2019-11-30 14:26:03 +0700

    Create branch University

---
+++ resources/js/pages/admin/University.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item">
+          <md-button  class="md-primary" @click="createStudent">Thêm</md-button>
+          <md-button  class="md-info" @click="refresh">Làm mới</md-button>
+          <md-button  class="md-danger" @click="removeManyStudent()">Xóa</md-button>
+        </div>
+        <div class="md-layout-item md-size-30">
+          <md-field >
+            <label>Tìm kiếm</label>
+            <md-input type="text" name="text" v-model="searchInput" @keyup.enter="$refs.datatable.refresh()"></md-input>
+            <md-icon>search</md-icon>
+          </md-field>
+        </div>
+      </div>
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Quản lí sinh viên</h4>
+            <p class="category">Here is a subtitle for this table</p>
+          </md-card-header>
+          <md-card-content>
+            <data-table :get-data="getData" ref="datatable">
+                <th class="col_checkbox">
+                  <md-checkbox :plain="true" v-model="selectedAll"></md-checkbox>
+                </th>
+                <th class="col_title_en">Tên đăng nhập</th>
+                <th class="col_title_jp">Email</th>
+                <th class="col_summary_en">Tên</th>
+                <th class="col_created_at">Ngày tạo</th>
+                <th class="col_tools">Công cụ</th>
+                <template slot="body" slot-scope="{ item, index }">
+                  <tr>
+                    <td class="text-center">
+                      <md-checkbox v-model="item.selected" @input="listenSelectRow"></md-checkbox>
+                    </td>
+                    <td class="text-center" v-html="item.name"></td>
+                    <td class="text-center" v-html="item.email"></td>
+                    <td class="text-center" v-html="item.full_name"></td>
+                    <td class="text-center" v-html="item.created_at"></td>
+                    <td class="text-center">
+                      <md-button class="md-info" @click="editStudent(item.id)">Sửa</md-button>
+                      <md-button class="md-danger" @click="removeOneStudent(item.id)">Xóa</md-button>
+                    </td>
+                  </tr>
+                </template>
+            </data-table>
+          </md-card-content>
+        </md-card>
+      </div>
+      <v-dialog/>
+      <UniversityModal @refresh="refresh()"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  SimpleTable,
+  OrderedTable
+} from '@/components'
+
+import rf from '../../requests/RequestFactory';
+import UniversityModal from '../../modals/University'
+
+export default{
+  components: {
+    OrderedTable,
+    SimpleTable,
+    UniversityModal
+  },
+  data () {
+    return {
+      searchInput: '',
+      selectedAll: false
+    }
+  },
+  methods: {
+    removeOneStudent(studentId) {
+      this.$modal.show('dialog', {
+        title: 'Cảnh báo!',
+        text: 'Bạn có chắc chắn muốn xóa ?',
+        buttons: [
+          {
+            title: 'Bỏ qua',
+            handler: () => {
+              this.$modal.hide('dialog');
+            }
+          },
+          {
+            title: 'Xác nhận',
+            default: true,
+            handler: () => {
+              return rf.getRequest('StudentRequest').removeOneStudent(studentId).then(() => {
+                this.$modal.hide('dialog');
+                this.$refs.datatable.refresh();
+                this.$toasted.show('Xóa sinh viên thành công!', {
+                  theme: 'bubble',
+                  position: 'top-right',
+                  duration : 1500,
+                  type: 'success'
+                });
+              });
+            }
+          },
+        ]
+      });
+    },
+    removeManyStudent() {
+        this.$modal.show('dialog', {
+          title: 'Cảnh báo!',
+          text: 'Bạn có chắc chắn muốn xóa ?',
+          buttons: [
+            {
+              title: 'Bỏ qua',
+              handler: () => {
+                this.$modal.hide('dialog');
+              }
+            },
+            {
+              title: 'Xác nhận',
+              default: true,
+              handler: () => {
+                const studentIds = this.$refs.datatable.rows.filter((row) => {
+                  return row.selected === true;
+                }).map(record => record.id);
+
+                return rf.getRequest('StudentRequest').removeManyStudents(studentIds).then(() => {
+                  this.$modal.hide('dialog');
+                  this.$refs.datatable.refresh();
+                  this.$toasted.show('Student removed successfully!', {
+                    theme: 'bubble',
+                    position: 'top-right',
+                    duration : 1500,
+                    type: 'success'
+                  });
+                });
+              }
+            },
+          ]
+        });
+      },
+      createStudent() {
+        this.$modal.show('university', {title: 'Thêm sinh viên'});
+      },
+      editStudent(studentId) {
+        this.$modal.show('student', {title: 'Sửa thông tin sinh viên', studentId: studentId});
+      },
+      listenSelectRow() {
+        if (!this.$refs.datatable) {
+          return true;
+        }
+
+        this.selectedAll = this.$refs.datatable.rows.filter(row => row.selected === true).length === this.$refs.datatable.rows.length;
+      },
+      getData (params) {
+        const meta = Object.assign({}, params, {
+          search: this.searchInput,
+        });
+        return rf.getRequest('UniversityRequest').getUniversities(meta);
+      },
+      refresh() {
+        this.isLoading = true;
+        this.$refs.datatable.refresh();
+        this.$refs.datatable.$on('DataTable:finish', () => {
+          this.isLoading = false;
+        });
+      },
+  },
+  watch: {
+    searchInput() {
+      this.$refs.datatable.refresh();
+    },
+    selectedAll() {
+      if (!this.$refs.datatable) {
+        return true;
+      }
+      this.$refs.datatable.rows.forEach((row) => {
+        this.$set(row, 'selected', this.selectedAll);
+      });
+    },
+  }
+}
+</script>
---
+++ resources/js/requests/UniversityRequest.js
@@ -0,0 +1,36 @@
+import BaseModelRequest from './BaseModelRequest';
+
+export default class UniversityRequest extends BaseModelRequest {
+  getUniversities(params) {
+    const url = '/admin/universities';
+    return this.get(url, params);
+  }
+
+  store(params) {
+
+    const url = '/admin/university';
+
+    return this.post(url, params);
+  }
+
+  show(universityId) {
+    const url = '/admin/university/' + universityId;
+    return this.get(url);
+  }
+
+  update(studentId, params) {
+    const url = '/admin/university/' + universityId;
+    return this.put(url, params);
+  }
+
+  removeOneUniversity(universityId) {
+    const url = '/admin/university/' + universityId;
+    return this.del(url);
+  }
+
+  removeManyUniversities(universityIds) {
+    const url = '/admin/universities';
+    return this.del(url, {ids: universityIds});
+  }
+
+}
