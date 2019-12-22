63fbe1f03dce3d9e7ded5447753f4f5bfecc1884
Hieu Do <minhhieuuet@gmail.com>
2019-12-22 12:12:08 +0700

    update transition

--- resources/js/pages/admin/Layout/Content.vue
+++ resources/js/pages/admin/Layout/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="fade" mode="out-in">
+  <transition name="slide-fade" mode="out-in">
     <router-view></router-view>
   </transition>
 </template>
@@ -7,16 +7,15 @@
 export default{}
 </script>
 <style>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity .1s
-  }
-
-  .fade-enter,
-  .fade-leave-to
-    /* .fade-leave-active in <2.1.8 */
-
-  {
-    opacity: 0
-  }
+.slide-fade-enter-active {
+  transition: all .2s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
